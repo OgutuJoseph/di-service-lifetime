1ebfd0e6e73b0a4c761dd80048360899118b894e
OgutuJoseph <onyango.joseph@yahoo.com>
2023-11-01 13:42:34 +0300

    Assign service implementations to local variables

--- DI_Service_Lifetime/DI_Service_Lifetime/Controllers/HomeController.cs
+++ DI_Service_Lifetime/DI_Service_Lifetime/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using DI_Service_Lifetime.Models;
+using DI_Service_Lifetime.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,11 +7,29 @@ namespace DI_Service_Lifetime.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IScopedGuidService _scoped1;
+        private readonly IScopedGuidService _scoped2;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ISingletonGuidService _singleton1;
+        private readonly ISingletonGuidService _singleton2;
+
+        private readonly ITransientGuidService _transient1;
+        private readonly ITransientGuidService _transient2;
+
+        public HomeController(IScopedGuidService scopedGuid1,
+            IScopedGuidService scopedGuid2,
+            ISingletonGuidService singleton1,
+            ISingletonGuidService singleton2,
+            ITransientGuidService transient1,
+            ITransientGuidService transient2
+            )
         {
-            _logger = logger;
+            _singleton1 = singleton1;
+            _singleton2 = singleton2;
+            _scoped1 = scopedGuid1;
+            _scoped2 = scopedGuid2;
+            _transient1 = transient1;
+            _transient2 = transient2;
         }
 
         public IActionResult Index()
